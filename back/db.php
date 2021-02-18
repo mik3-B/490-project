83e263fb0b1d676905e4ec8bc4b82deb3f9e1a1a
Michael Bautista <78510397+mik3-B@users.noreply.github.com>
2021-02-18 12:45:12 -0500

    Update db.php

--- back/db.php
+++ back/db.php
@@ -1,10 +1,10 @@
 <?php
 error_reporting(0);
 ini_set('display_errors',0);
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','profile');
+define('DB_HOST','us-cdbr-east-03.cleardb.com');
+define('DB_USER','b7128fd89bab29');
+define('DB_PASS','3a256df6');
+define('DB_NAME','heroku_762e10c496e19f9');
 $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8";
 $db = new PDO($dsn,DB_USER,DB_PASS);
 
